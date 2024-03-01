8faebeab45b13ba52c75450a089191d2501354e3
Peter Zhang <pzhang04@stanford.edu>
2024-02-29 21:20:15 -0800

    Initial Commit

---
+++ Quture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quture
+//
+//  Created by Peter Zhang on 2024/2/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quture/QutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  QutureApp.swift
+//  Quture
+//
+//  Created by Peter Zhang on 2024/2/29.
+//
+
+import SwiftUI
+
+@main
+struct QutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
